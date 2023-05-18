9972a02df228f963dd63ded5ff34f9208c83476f
davidcoats <david@getsafetycone.com>
2023-05-17 22:46:39 -0700

    Initial commit.

---
+++ source/R5T.L0044.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0044.Construction
+Construction console project for the R5T.L0044 library.
---
+++ source/R5T.L0044/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0044
+{
+	/// <summary>
+	/// Project name operations library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
